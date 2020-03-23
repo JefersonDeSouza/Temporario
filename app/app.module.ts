af5c449bb7ef2e9524c8834ff2f52faf4d60ffe6
dnacbar <douglasnac1@hotmail.com>
2020-03-23 17:52:38 -0300

    PROTOTIPO GERENTE-CLIENTE

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SortableModule } from 'ngx-bootstrap/sortable';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SortableModule.forRoot(), FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/model/clienteModel.ts
@@ -0,0 +1,5 @@
+export class ClienteModel {
+	CodCliente: string;
+	Documento: string;
+	Nome: string;
+}
